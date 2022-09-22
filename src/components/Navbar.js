408de2cd8f3723a3970ea1955a0035c6105dffb4
Kris <kris@syevens.com>
2022-09-21 22:44:00 -0500

    Update Commit - Nav and About Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,68 +1,45 @@
+import logo from "../images/logo.svg";
+import { pageLinks, externalLinks } from "../data";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
-          <img src="./images/logo.svg" className="nav-logo" alt="backroads" />
+          <img src={logo} className="nav-logo" alt="backroads" />
           <button type="button" className="nav-toggle" id="nav-toggle">
             <i className="fas fa-bars"></i>
           </button>
         </div>
         {/* <!-- left this comment on purpose --> */}
         <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link">
-              home
-            </a>
-          </li>
-
-          <li>
-            <a href="#about" className="nav-link">
-              about
-            </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link">
-              services
-            </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link">
-              tours
-            </a>
-          </li>
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
         </ul>
 
         <ul className="nav-icons">
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-            >
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li>
+          {externalLinks.map((link) => {
+            const { id, href, iconName } = link;
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="nav-icon"
+                >
+                  <i className={iconName}></i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+
+export const externalLinks = [
+  { id: 1, href: "https://www.facebook.com", iconName: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", iconName: "fab fa-twitter" },
+  {
+    id: 3,
+    href: "https://www.squarespace.com",
+    iconName: "fab fa-squarespace",
+  },
+];
