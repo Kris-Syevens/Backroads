21c0a662d07f32ea04fb9e39dafd08c81466c869
Kris <kris@syevens.com>
2022-09-22 23:00:43 -0500

    Update Commit -Title Services  and  About Update

--- src/data.js
+++ src/data.js
@@ -14,3 +14,24 @@ export const externalLinks = [
     iconName: "fab fa-squarespace",
   },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
