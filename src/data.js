91fa8f5f90b6165a2764b0c3960e78d62361f2b3
Kris <kris@syevens.com>
2022-09-24 00:56:31 -0500

    Update Commit -Title Services  and  About Update

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -33,5 +38,48 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "amazing comfort",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    img: tour1,
+    title: "Tibet Adventure",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "august 26th, 2020",
+    location: "china",
+    duration: "6 days",
+    cost: "from $2100",
+  },
+  {
+    id: 2,
+    img: tour2,
+    title: "best of java",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "october 1th, 2020",
+    location: "indonesia",
+    duration: "11 days",
+    cost: "from $1400",
+  },
+  {
+    id: 3,
+    img: tour3,
+    title: "explore hong kong",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "september 15th, 2020",
+    location: "hong kong",
+    duration: "8 days",
+    cost: "from $5000",
+  },
+  {
+    id: 4,
+    img: tour4,
+    title: "kenya highlights",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    date: "december 5th, 2019",
+    location: " kenya",
+    duration: "20 days",
+    cost: "from $3300",
   },
 ];
