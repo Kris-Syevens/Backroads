a37c58ed8975c1d9e5cf0e3b58a1d242c88d13b7
Kris <kris@syevens.com>
2022-09-24 01:04:05 -0500

    Update Commit - Refactored Services Components

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,6 @@
 import Title from "./Title";
 import { services } from "../data";
+import ServiceItem from "./ServiceItem";
 
 const Services = () => {
   return (
@@ -7,18 +8,8 @@ const Services = () => {
       <Title title="our" subTitle="services" />
 
       <div className="section-center services-center">
-        {services.map(({ id, icon, title, text }) => {
-          return (
-            <article key={id} className="service">
-              <span className="service-icon">
-                <i className={icon}></i>
-              </span>
-              <div className="service-info">
-                <h4 className="service-title">{title}</h4>
-                <p className="service-text">{text}</p>
-              </div>
-            </article>
-          );
+        {services.map((service) => {
+          return <ServiceItem key={service.id} {...service} />;
         })}
       </div>
     </section>
